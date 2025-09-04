620d4ffa98584d2157a72e94bf17e6e73aa5a6d5
RASH <jestinjosevarampel@gmail.com>
2025-09-04 23:51:00 +0530

    Fix: case-sensitive import for User model

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app=express();
 
 app.use(cors({
-  origin: "https://ecommerceapp-7.onrender.com",
+  origin: "https://ecommerceapp-11.onrender.com",
   credentials: true
 }));
 app.use(express.json());
